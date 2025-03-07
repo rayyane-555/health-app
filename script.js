a6bf8562c5f803da135d3d2bda867543349b05f7
rayyane-555 <rexorayyane@gmail.com>
2025-03-07 13:27:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,410 @@
+function goToScreen1() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen1').classList.remove('hidden');
+}
+
+function goToScreen2() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen2').classList.remove('hidden');
+}
+
+function goToScreen3() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen3').classList.remove('hidden');
+}
+
+function goToScreen4() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen4').classList.remove('hidden'); 
+}
+
+function goToScreen5() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen5').classList.remove('hidden'); 
+}
+
+function goToScreen6() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen6').classList.remove('hidden');
+}
+
+function goToScreen7() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen7').classList.remove('hidden'); 
+}
+
+function goToScreen8() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen8').classList.remove('hidden'); 
+}
+
+function goToScreen9() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen9').classList.remove('hidden'); 
+}
+function goToScreen10() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen10').classList.remove('hidden'); 
+}
+function goToScreen11() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen11').classList.remove('hidden'); 
+}
+function goToScreen12() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen12').classList.remove('hidden'); 
+}
+function goToScreen13() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen13').classList.remove('hidden'); 
+}
+function goToScreen14() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen14').classList.remove('hidden'); 
+}
+function goToScreen15() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen15').classList.remove('hidden'); 
+}
+function goToScreen16() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen16').classList.remove('hidden'); 
+}
+function goToScreen17() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen17').classList.remove('hidden'); 
+}
+function goToScreen18() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen18').classList.remove('hidden'); 
+}
+function goToScreen19() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen19').classList.remove('hidden'); 
+}
+function goToScreen20() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen20').classList.remove('hidden'); 
+}
+function goToScreen21() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen21').classList.remove('hidden'); 
+}
+function goToScreen22() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen22').classList.remove('hidden'); 
+}
+function goToScreen23() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen23').classList.remove('hidden'); 
+}
+function goToScreen24() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen24').classList.remove('hidden'); 
+}
+function goToScreen25() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen25').classList.remove('hidden'); 
+}
+function goToScreen26() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen26').classList.remove('hidden'); 
+}
+function goToScreen27() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen27').classList.remove('hidden'); 
+}
+function goToScreen28() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen28').classList.remove('hidden'); 
+}
+function goToScreen29() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen29').classList.remove('hidden'); 
+}
+function goToScreen30() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen30').classList.remove('hidden'); 
+}
+function goToScreen31() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen31').classList.remove('hidden'); 
+}
+function goToScreen32() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen32').classList.remove('hidden'); 
+}
+function goToScreen33() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen33').classList.remove('hidden'); 
+}
+function goToScreen34() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen34').classList.remove('hidden'); 
+}
+function goToScreen35() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen35').classList.remove('hidden'); 
+}
+function goToScreen36() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen36').classList.remove('hidden'); 
+}
+function goToScreen37() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen37').classList.remove('hidden'); 
+}
+function goToScreen38() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen38').classList.remove('hidden'); 
+}
+function goToScreen39() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen39').classList.remove('hidden'); 
+}
+function goToScreen40() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen40').classList.remove('hidden'); 
+}
+function goToScreen41() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen41').classList.remove('hidden'); 
+}
+function goToScreen42() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen42').classList.remove('hidden'); 
+}
+function goToScreen43() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen43').classList.remove('hidden'); 
+}
+function goToScreen44() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen44').classList.remove('hidden'); 
+}
+function goToScreen45() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen45').classList.remove('hidden'); 
+}
+function goToScreen46() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen46').classList.remove('hidden'); 
+}
+function goToScreen47() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen47').classList.remove('hidden'); 
+}
+function goToScreen48() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen48').classList.remove('hidden'); 
+}
+function goToScreen49() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen49').classList.remove('hidden'); 
+}
+function goToScreen50() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen50').classList.remove('hidden'); 
+}
+function goToScreen51() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen51').classList.remove('hidden'); 
+}
+function goToScreen52() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen52').classList.remove('hidden'); 
+}
+function goToScreen53() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen53').classList.remove('hidden'); 
+}
+function goToScreen54() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen54').classList.remove('hidden'); 
+}
+function goToScreen55() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen55').classList.remove('hidden'); 
+}
+function goToScreen56() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen56').classList.remove('hidden'); 
+}
+function goToScreen57() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen57').classList.remove('hidden'); 
+}
+function goToScreen58() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen58').classList.remove('hidden'); 
+}
+function goToScreen59() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen59').classList.remove('hidden'); 
+}
+function goToScreen60() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen60').classList.remove('hidden'); 
+}
+function goToScreen61() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen61').classList.remove('hidden'); 
+}
+function goToScreen62() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen62').classList.remove('hidden'); 
+}
+function goToScreen63() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen63').classList.remove('hidden'); 
+}
+function goToScreen64() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen64').classList.remove('hidden'); 
+}
+function goToScreen65() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen65').classList.remove('hidden'); 
+}
+function goToScreen66() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen66').classList.remove('hidden'); 
+}
+function goToScreen67() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen67').classList.remove('hidden'); 
+}
+function goToScreen68() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen68').classList.remove('hidden'); 
+}
+function goToScreen69() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen69').classList.remove('hidden'); 
+}
+function goToScreen70() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen70').classList.remove('hidden'); 
+}
+function goToScreen71() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen71').classList.remove('hidden'); 
+}
+function goToScreen72() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen72').classList.remove('hidden'); 
+}
+function goToScreen73() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen73').classList.remove('hidden'); 
+}
+function goToScreen74() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen74').classList.remove('hidden'); 
+}
+function goToScreen75() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen75').classList.remove('hidden'); 
+}
+function goToScreen76() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen76').classList.remove('hidden'); 
+}
+function goToScreen77() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen77').classList.remove('hidden'); 
+}
+function goToScreen78() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen78').classList.remove('hidden'); 
+}
+function goToScreen79() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen79').classList.remove('hidden'); 
+}
+function goToScreen80() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen80').classList.remove('hidden'); 
+}
+function goToScreen81() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen81').classList.remove('hidden'); 
+}
+function goToScreen82() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen82').classList.remove('hidden'); 
+}
+function goToScreen83() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen83').classList.remove('hidden'); 
+}
+function goToScreen84() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen84').classList.remove('hidden'); 
+}
+function goToScreen85() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen85').classList.remove('hidden'); 
+}
+function goToScreen86() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen86').classList.remove('hidden'); 
+}
+function goToScreen87() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen87').classList.remove('hidden'); 
+}
+function goToScreen88() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen88').classList.remove('hidden'); 
+}
+function goToScreen89() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen89').classList.remove('hidden'); 
+}
+function goToScreen90() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen90').classList.remove('hidden'); 
+}
+function goToScreen91() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen91').classList.remove('hidden'); 
+}
+function goToScreen92() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen92').classList.remove('hidden'); 
+}
+function goToScreen93() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen93').classList.remove('hidden'); 
+}
+function goToScreen94() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen94').classList.remove('hidden'); 
+}
+function goToScreen95() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen95').classList.remove('hidden'); 
+}
+function goToScreen96() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen96').classList.remove('hidden'); 
+}
+function goToScreen97() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen97').classList.remove('hidden'); 
+}
+function goToScreen98() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen98').classList.remove('hidden'); 
+}
+function goToScreen99() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen99').classList.remove('hidden'); 
+}
+function goToScreen100() {
+  document.querySelectorAll('.screen').forEach(screen => screen.classList.add('hidden'));
+  document.getElementById('screen100').classList.remove('hidden'); 
+}
+
+
